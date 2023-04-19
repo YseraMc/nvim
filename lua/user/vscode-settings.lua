207700d85d0034e8da1d5ae8fdadc18a36e756c9
chawata-ubuntu <cha39881@gmail.com>
2023-04-20 01:00:50 +0900

    Update VScode settings

--- lua/user/vscode-settings.lua
+++ lua/user/vscode-settings.lua
@@ -6,8 +6,20 @@ vim.opt.clipboard:append({ "unnamedplus" })
 --
 vim.g.mapleader = " "
 
--- xキーで消した文字をレジスタに入れない
+vim.keymap.set("n", "<Esc><Esc>", ":nohlsearch<CR>", { noremap = true })
 vim.keymap.set("n", "x", '"_x', { noremap = true })
+
+-- VSCodeコマンド
+vim.keymap.set("n", "<leader>o", "<Cmd>call VSCodeNotify('workbench.action.quickOpen')<CR>")
+vim.keymap.set("n", "H", "<Cmd>call VSCodeNotify('workbench.action.previousEditor')<CR>")
+vim.keymap.set("n", "L", "<Cmd>call VSCodeNotify('workbench.action.nextEditor')<CR>")
+vim.keymap.set("n", "<leader>b", "<Cmd>call VSCodeNotify('workbench.action.toggleSidebarVisibility')<CR>")
+vim.keymap.set("n", "<leader>e", "<Cmd>call VSCodeNotify('workbench.view.explorer')<CR>")
+vim.keymap.set("n", "<leader>g", "<Cmd>call VSCodeNotify('workbench.view.search')<CR>")
+vim.keymap.set("n", "<C-t>", "<Cmd>call VSCodeNotify('workbench.action.terminal.toggleTerminal')<CR>")
+
+vim.keymap.set("n", "gh", "<Cmd>call VSCodeNotify('editor.action.showHover')<CR>")
+vim.keymap.set("n", "gd", "<Cmd>call VSCodeNotify('editor.action.goToDeclaration')<CR>")
 
 --
 -- plugin
