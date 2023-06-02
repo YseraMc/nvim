4351294e07058837f7a12eea2c2dccbff316cd92
chappieM-lubuntu <cha39881@gmail.com>
2023-06-03 00:01:27 +0900

    add select buffer keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -23,33 +23,12 @@ vim.keymap.set("n", "<leader>dc", ":tabnew ~/.config/cspell/cspell.json<CR>")
 vim.keymap.set("n", "<leader>g", ":Telescope live_grep<CR>", { noremap = true })
 
 -- fzf
-vim.keymap.set("n", "<leader>f", ":Telescope find_files<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>o", ":Telescope find_files<CR>", { noremap = true })
 
 -- quickfix
 vim.keymap.set("n", "tq", ":Telescope quickfix<CR>", { noremap = true })
 
 -- バッファ
 vim.keymap.set("n", "<leader>b", ":Telescope buffers<CR>", { noremap = true })
-
--- Term
-vim.keymap.set("n", "<C-\\>", ":ToggleTerm direction=float<CR>", { noremap = true })
-function _G.set_terminal_keymaps()
-	local opts = { buffer = 0 }
-	vim.keymap.set("t", "<C-\\>", [[<C-\><C-n> :ToggleTerm<CR>]], opts)
-	vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
-	vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
-	vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
-	vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
-	vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
-end
-
--- if you only want these mappings for toggle term use term://*toggleterm#* instead
-vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
-
--- hop
-vim.keymap.set("n", "<leader>h", ":HopChar1<CR>", { noremap = true })
-vim.keymap.set("n", "<leader>l", ":HopLine<CR>", { noremap = true })
-
--- markdown
-vim.keymap.set("n", "<leader>mp", ":MarkdownPreview<CR>", { noremap = true })
-vim.keymap.set("n", "<leader>mps", ":MarkdownPreviewStop<CR>", { noremap = true })
+vim.keymap.set("n", "<C-h>", ":bprev<CR>", { noremap = true })
+vim.keymap.set("n", "<C-l>", ":bnext<CR>", { noremap = true })
