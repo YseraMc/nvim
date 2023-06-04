40632f7e6d2644ff17791988c96caaa1caa852ed
chappiem <cha39881@gmail.com>
2023-06-04 21:26:50 +0900

    feat: keymap $ and ^

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -9,9 +9,11 @@
 vim.g.mapleader = " "
 
 vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
--- vim.keymap.set("n", ";", ":", { noremap = true })
 vim.keymap.set("n", "x", '"_x', { noremap = true })
 vim.keymap.set("n", "<Esc><Esc>", ":nohlsearch<CR>", { noremap = true })
+
+vim.keymap.set("n", "<leader>h", "^", {noremap =true})
+vim.keymap.set("n", "<leader>l", "$", {noremap =true})
 
 -- dict
 vim.keymap.set("n", "<leader>dc", ":tabnew ~/.config/cspell/cspell.json<CR>")
