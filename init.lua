e7c75e4450d73dd9cb1c89f855f2fc86e5da911e
chappiem <cha39881@gmail.com>
2023-06-07 00:44:53 +0900

    refactor: 整理

--- init.lua
+++ init.lua
@@ -1,15 +1,16 @@
+require("user.options")
+require("user.keymaps")
+require("user.plugins")
+require("user.masson")
+require("user.lsp")
+require("user.null-ls")
+require("user.formatter")
+require("user.cmp")
+require("user.snippy")
+require("user.treesitter")
+
 if not vim.g.vscode then
-	require("user.options")
-	require("user.keymaps")
-	require("user.plugins")
-	require("user.masson")
-	require("user.lsp")
-	require("user.null-ls")
-	require("user.formatter")
-	require("user.cmp")
-	require("user.snippy")
-	require("user.treesitter")
-	require("user.color-theme")
+	--  vim only settings
 else
 	require("user.vscode-settings")
 end
--- lua/user/vscode-settings.lua
+++ lua/user/vscode-settings.lua
@@ -1,14 +1,3 @@
--- クリップボード連携
-vim.opt.clipboard:append({ "unnamedplus" })
-
---
--- keymap
---
-vim.g.mapleader = " "
-
-vim.keymap.set("n", "<Esc><Esc>", ":nohlsearch<CR>", { noremap = true })
-vim.keymap.set("n", "x", '"_x', { noremap = true })
-
 -- VSCodeコマンド
 vim.keymap.set("n", "<leader>o", "<Cmd>call VSCodeNotify('workbench.action.quickOpen')<CR>")
 vim.keymap.set("n", "<leader>c", "<Cmd>call VSCodeNotify('workbench.action.showCommands')<CR>")
@@ -24,26 +13,3 @@ vim.keymap.set("n", "<leader>g", "<Cmd>call VSCodeNotify('workbench.view.search'
 
 vim.keymap.set("n", "gh", "<Cmd>call VSCodeNotify('editor.action.showHover')<CR>")
 vim.keymap.set("n", "gd", "<Cmd>call VSCodeNotify('editor.action.goToDeclaration')<CR>")
-
---
--- plugin
---
-require("packer").startup(function(use)
-	use("wbthomason/packer.nvim")
-	use("numToStr/Comment.nvim")
-	use("phaazon/hop.nvim")
-	use("machakann/vim-sandwich")
-	use("h-hg/fcitx.nvim")
-end)
-
--- Packerオートコンパイル
-vim.api.nvim_create_autocmd("BufWritePost", {
-	pattern = { "plugins.lua" },
-	command = "PackerCompile",
-})
-
-require("Comment").setup({})
-require("hop").setup({})
-
-vim.keymap.set("n", "<leader>h", ":HopChar1<CR>", { noremap = true })
-vim.keymap.set("n", "<leader>l", ":HopLine<CR>", { noremap = true })
