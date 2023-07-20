7df2214f8949f8035f643b016809a13bdbadd864
chappiem <cha39881@gmail.com>
2023-07-21 04:58:32 +0900

    feat: local config

--- init.lua
+++ init.lua
@@ -1,13 +1,13 @@
-require("user.options")
-require("user.keymaps")
-require("user.plugins")
-require("user.masson")
-require("user.lsp")
-require("user.null-ls")
-require("user.formatter")
-require("user.cmp")
-require("user.snippy")
-require("user.treesitter")
+require("base.options")
+require("base.keymaps")
+require("base.plugins")
+require("base.masson")
+require("base.lsp")
+require("base.null-ls")
+require("base.formatter")
+require("base.cmp")
+require("base.snippy")
+require("base.treesitter")
 
 if not vim.g.vscode then
 	-- osc52
@@ -18,5 +18,35 @@ if not vim.g.vscode then
 	end
 	vim.api.nvim_create_autocmd("TextYankPost", { callback = copy })
 else
-	require("user.vscode-settings")
+	require("base.vscode-settings")
 end
+
+--
+-- local固有の設定ファイルを読み込む
+--
+
+-- 特定のディレクトリ以下のファイルを再帰的に取得する関数
+local function getFilesInDirectory(dir)
+	local files = {}
+	local pfile = io.popen('dir "' .. dir .. '" /b /a-d')
+	for file in pfile:lines() do
+		table.insert(files, dir .. "/" .. file)
+	end
+	pfile:close()
+	return files
+end
+
+-- 特定のディレクトリ以下のLuaファイルをすべてrequireする関数
+local function requireAllLuaFilesInDirectory(dir)
+	local files = getFilesInDirectory(dir)
+
+	for _, file in ipairs(files) do
+		if file:match("%.lua$") then -- .lua拡張子を持つ場合のみ処理します
+			local moduleName = file:match("(.+)%..+$") -- 拡張子を取り除いてモジュール名を取得
+			require(moduleName)
+		end
+	end
+end
+
+local directoryPath = "./local"
+requireAllLuaFilesInDirectory(directoryPath)
